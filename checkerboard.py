d485e4bd4c85f19b949d4e3b3f3e2114c5def838
shilpa-kotian <66106841+shilpa-kotian@users.noreply.github.com>
2020-06-21 16:47:23 +0530

    new submission

--- checkerboard.py
+++ checkerboard.py
@@ -1,12 +1,11 @@
 
 import numpy as np
 import cv2
-n=int(50)
-b=np.zeros([400,400],dtype='uint8')
-for i in range(n,400,n*2):
- for j in range(n,400,n*2):
-  b[j-n:j,i-n:i]=255
-  b[j:j+n,i:i+n]=255
+b=np.zeros([240,240],dtype='uint8')
+for i in range(30,400,60):
+ for j in range(30,400,60):
+  b[j-30:j,i-30:i]=255
+  b[j:j+30,i:i+30]=255
 cv2.imshow('checker board 8*8',b)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
